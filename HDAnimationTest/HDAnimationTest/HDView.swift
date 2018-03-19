d6e339ef0e63f18240a108450257723874784a6a
HJddd <ddd@HJddddeiMac.local>
2018-03-19 16:43:51 +0800

    pods pop

--- HDAnimationTest/HDAnimationTest/HDView.swift
+++ HDAnimationTest/HDAnimationTest/HDView.swift
@@ -16,21 +16,30 @@ class HDView: UIView {
     override func draw(_ rect: CGRect) {
         // Drawing code
         
+        //五角星⭐️
+        let centerX = rect.size.width/2
+        let centerY = rect.size.height/2
+        
+        
         //矩形贝塞尔曲线
-        let bezierPath_rect = UIBezierPath(rect: CGRect(x: 30, y: 50, width: 100, height: 100))
-        bezierPath_rect.move(to: CGPoint(x: 60, y: 60))
-        bezierPath_rect.addLine(to: CGPoint(x: 80, y: 80))
-        bezierPath_rect.addLine(to: CGPoint(x: 60, y: 90))
-        bezierPath_rect.lineCapStyle = .butt   //端点类型
-        bezierPath_rect.lineJoinStyle = .miter //线条连接类型
-        bezierPath_rect.miterLimit = 1
-        bezierPath_rect.lineWidth = 3
+        let bezierPath_rec = UIBezierPath(rect: CGRect(x: 30, y: 50, width: 100, height: 100))
+        bezierPath_rec.move(to: CGPoint(x: 30, y: 50))
+        bezierPath_rec.addLine(to: CGPoint(x: 63, y: 83))
+        bezierPath_rec.addLine(to: CGPoint(x: 78, y:50 ))
+        bezierPath_rec.addLine(to: CGPoint(x: 96, y: 83))
+        bezierPath_rec.addLine(to: CGPoint(x: 129, y: 83))
+        bezierPath_rec.addLine(to: CGPoint(x: 96, y: 116))
+   
+        bezierPath_rec.lineCapStyle = .butt   //端点类型
+        bezierPath_rec.lineJoinStyle = .miter //线条连接类型
+        bezierPath_rec.miterLimit = 1
+        bezierPath_rec.lineWidth = 3
         
         //圆弧贝塞尔曲线
         let bezierPath_arc = UIBezierPath(arcCenter: CGPoint(x: 0, y: 400), radius: 50, startAngle: CGFloat(0), endAngle: CGFloat(M_PI/3), clockwise: true)
         bezierPath_arc.lineWidth = 3
         
-        //添加二次三次贝塞尔曲线
+        //添加多次贝塞尔曲线
         let bezierPathMy = UIBezierPath()
         bezierPathMy.lineWidth = 3
         bezierPathMy.move(to: CGPoint(x: 10, y: 520))
@@ -40,10 +49,12 @@ class HDView: UIView {
         bezierPathMy.addArc(withCenter: CGPoint(x: 300, y:400), radius: 50, startAngle: 0, endAngle: CGFloat(M_PI*2), clockwise: true)
         bezierPathMy.move(to: CGPoint(x: 20, y:520))
         bezierPathMy.addLine(to: CGPoint(x: 40, y:520))
+
+        UIColor.gray.set()
+        bezierPath_rec.fill()
         
         UIColor.black.set()
-        bezierPathMy.stroke()
-        
+        bezierPath_rec.stroke()
         
         let aniLayer = CALayer()
         aniLayer.backgroundColor = UIColor.red.cgColor
@@ -54,7 +65,7 @@ class HDView: UIView {
         
         let keyFrameAnima = CAKeyframeAnimation(keyPath: "position")
         keyFrameAnima.repeatCount = Float(NSIntegerMax)
-        keyFrameAnima.path = bezierPathMy.cgPath
+        keyFrameAnima.path = bezierPath_rec.cgPath
         keyFrameAnima.duration = 15
         keyFrameAnima.beginTime = CACurrentMediaTime() + 1
         aniLayer.add(keyFrameAnima, forKey: "keyFrameAnimation")
