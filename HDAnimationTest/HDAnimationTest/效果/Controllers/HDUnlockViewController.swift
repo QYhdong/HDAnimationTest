37fb366b434e7a891ec0a90dfe77ef86cb1e47ad
QYhdong <809184372@qq.com>
2018-04-19 17:29:09 +0800

    commit CAGradientLayerImage

---
+++ HDAnimationTest/HDAnimationTest/效果/Controllers/HDUnlockViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HDUnlockViewController.swift
+//  HDAnimationTest
+//
+//  Created by HJddd on 2018/4/19.
+//  Copyright © 2018年 HJddd. All rights reserved.
+//
+
+import UIKit
+
+class HDUnlockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
